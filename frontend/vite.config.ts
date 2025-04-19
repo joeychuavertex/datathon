a5e3fd63739b86ef1f22005a206f31ac421a68bc
joeychua2020 <joeychua2020@outlook.com>
2025-04-19 13:49:03 +0800

    reconfigure vercel deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +11,20 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: true,
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'mantine-vendor': ['@mantine/core', '@mantine/hooks', '@mantine/notifications'],
+        },
+      },
+    },
   },
   base: '/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 }) 
